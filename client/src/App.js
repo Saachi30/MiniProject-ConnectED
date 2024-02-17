eb50fd8226a2104b75db31e6bcfda9488fe6bab1
Abhishek Sharma <as327209@gmail.com>
2024-02-17 14:41:42 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import LandingPage from './components/LandingPage/LandingPage';
-import Login from './components/LoginPage/Login';
-import Choice from './components/ChoicePage/Choice';
+import logo from "./logo.svg";
+import "./App.css";
+import LandingPage from "./components/LandingPage/LandingPage";
+import Login from "./components/LoginPage/Login";
+import Choice from "./components/ChoicePage/Choice";
+import StudentPage from "./components/StudentPage/StudentPage";
 
 function App() {
   return (
     <div className="App">
       <div class="landingPage">
-      <div class="nav">
-        <div class="logo">
-          <div class="loggo"></div>
-          <h2 class="logoName">ConnectED</h2>
+        <div class="nav">
+          <div class="logo">
+            <div class="loggo"></div>
+            <h2 class="logoName">ConnectED</h2>
+          </div>
+          <button class="Login">Login/ Sign up</button>
         </div>
-        <button class="Login">Login/ Sign up</button>
+
+        <LandingPage />
+        <Choice />
+        <Login />
+        <StudentPage />
       </div>
-
-      <LandingPage/>
-      <Choice/>
-      <Login/>
-    </div>
-
-    
     </div>
   );
 }
