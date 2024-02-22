8458504afc4054e86be39d8303972769c35b18ad
Abhishek Sharma <as327209@gmail.com>
2024-02-22 23:29:17 +0530

    first commit

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,21 +1,54 @@
-import React from 'react'
-import './LandingPage.css'
-import ArrowForwardRoundedIcon from '@mui/icons-material/ArrowForwardRounded';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useRef } from "react";
+import Typed from "typed.js";
+import "./LandingPage.css";
+import ArrowForwardRoundedIcon from "@mui/icons-material/ArrowForwardRounded";
+import { Link } from "react-router-dom";
 const LandingPage = () => {
+  const typedTextRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      stringsElement: "#typed-strings",
+      typeSpeed: 50, // typing speed in milliseconds
+      loop: true, // loop the animation
+    };
+
+    const typed = new Typed(typedTextRef.current, options);
+
+    return () => {
+      typed.destroy(); // cleanup
+    };
+  }, []);
+
   return (
     <div className="mainLanding">
-    <button className="Loginbtn"><Link to='/choice' style={{textDecoration:"none",color:"#11113a"}}>Login/Sign up</Link></button>
-        <div className='outerdiv'>
-            <div className='leftLanding'>
-                <h1>Flexible Connections for your Educational journey</h1>
-                <p>Lorem ipsum praesent ac massa at ligula reet est iaculis. Vivamus est mist aliquet elit ac nisl. Lorem ipsum praesent ac massa at ligula reet est iaculis. Vivamus est mist.</p>
-                <button id='abt-btn' className='btn1'>Read more about us <ArrowForwardRoundedIcon style={{margin:"0px 6px"}}/></button>
-            </div>
-            <div className='rightLanding'></div>
-        </div>
-    </div>
-  )
-}
+      <button className="Loginbtn">
+        <Link to="/choice" style={{ textDecoration: "none", color: "#11113a" }}>
+          Login/Sign up
+        </Link>
+      </button>
+      <div className="outerdiv">
+        <div className="leftLanding">
+          <h1 ref={typedTextRef}></h1>
+          <div id="typed-strings" style={{ display: "none" }}>
+            <span>Flexible Connections for your Educational journey</span>
+            <span>Machine Learning</span>
+          </div>
 
-export default LandingPage
+          <p>
+            Lorem ipsum praesent ac massa at ligula reet est iaculis. Vivamus
+            est mist aliquet elit ac nisl. Lorem ipsum praesent ac massa at
+            ligula reet est iaculis. Vivamus est mist.
+          </p>
+          <button id="abt-btn" className="btn1">
+            Read more about us{" "}
+            <ArrowForwardRoundedIcon style={{ margin: "0px 6px" }} />
+          </button>
+        </div>
+        <div className="rightLanding"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
