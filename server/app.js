d0f7c302645314463a54e62bd4a9919f43f05722
Saachi <peswanisaachi@gmail.com>
2024-02-23 13:17:22 +0530

    yo

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
 dotenv.config();
 
 const app = express();
@@ -10,6 +12,14 @@ app.use(bodyParser.json());
 
 const port = 8000;
 
+const URL="mongodb://127.0.0.1:27017/connectED";
+    try{
+       await mongoose.connect(URL, {useUnifiedTopology: true, useNewUrlParser: true});
+       console.log("Database connected successfully");
+    }
+    catch(error){
+        console.log(error);
+    }
 app.listen(port, () => {
     console.log("Listening on port");
 });
